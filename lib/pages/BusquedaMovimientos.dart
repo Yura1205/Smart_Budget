1d864badf2de5cc96750f2452f7da05941adbcf5
DavidSanchez055 <davidf.sanchezm@una.edu.co>
2024-05-26 06:44:54 -0500

    Primer avance página Búsqueda

--- lib/pages/BusquedaMovimientos.dart
+++ lib/pages/BusquedaMovimientos.dart
@@ -17,7 +17,31 @@ class _Busqueda extends State<Busqueda> {
         title: Text("Búsqueda de Movimientos"),
       ),
       body: Center(
-        child: Text("Búsqueda de Movimientos"),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 15),
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: TextField(
+                  style:
+                      TextStyle(color: Colors.white), // Set text color to white
+                  decoration: InputDecoration(
+                    hintText: "Buscar",
+                    prefixIcon: Icon(Icons.search, color: Colors.white),
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 95, 96, 133),
+                    hintStyle: TextStyle(color: Colors.white),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     ));
   }
