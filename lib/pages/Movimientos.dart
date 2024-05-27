00db97c58bbc3e73ddf4d0edec325792916975c0
DavidSanchez055 <davidf.sanchezm@una.edu.co>
2024-05-26 22:20:15 -0500

    FrontEnd Movements Page

--- lib/pages/Movimientos.dart
+++ lib/pages/Movimientos.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class Movimientos extends StatefulWidget {
@@ -29,9 +31,18 @@ class _MovimientosState extends State<Movimientos> {
                   Expanded(
                     child: ListView(
                       children: [
-                        _buildMovimientoItem("Movimiento 1", "Descripción del movimiento 1", "\$100.00"),
-                        _buildMovimientoItem("Movimiento 2", "Descripción del movimiento 2", "\$200.00"),
-                        _buildMovimientoItem("Movimiento 3", "Descripción del movimiento 3", "\$300.00"),
+                        _buildMovimientoItem("Movimiento 1",
+                            "Descripción del movimiento 1", "\$100.00"),
+                        _buildMovimientoItem("Movimiento 2",
+                            "Descripción del movimiento 2", "\$200.00"),
+                        _buildMovimientoItem("Movimiento 3",
+                            "Descripción del movimiento 3", "\$300.00"),
+                        _buildMovimientoItem("Movimiento 4",
+                            "Descripción del movimiento 4", "\$400.00"),
+                        _buildMovimientoItem("Movimiento 5",
+                            "Descripción del movimiento 5", "\$500.00"),
+                        _buildMovimientoItem("Movimiento 6",
+                            "Descripción del movimiento 6", "\$600.00"),
                       ],
                     ),
                   ),
@@ -44,15 +55,18 @@ class _MovimientosState extends State<Movimientos> {
                   child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       shape: CircleBorder(), // Make the button circular
-                      padding: EdgeInsets.all(20), // Padding around the button icon
-                      backgroundColor: Color(0xFFF2003D), // Assign color to the ElevatedButton
+                      padding:
+                          EdgeInsets.all(20), // Padding around the button icon
+                      backgroundColor:
+                          Colors.white, // Assign color to the ElevatedButton
                     ),
                     onPressed: () {
-                      // Add your logic here
+                      openCreateDialog();
                     },
                     child: Icon(
                       Icons.add,
-                      color: Colors.white, // Set the color of the Icon to white
+                      color: Color(
+                          0xFFF2003D), // Set the color of the Icon to white
                       size: 30, // Adjust the size of the icon
                     ),
                   ),
@@ -68,49 +82,160 @@ class _MovimientosState extends State<Movimientos> {
   Widget _buildMovimientoItem(String title, String subtitle, String value) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 20),
-      child: Expanded(
-        child: ListTile(
-          title: Text(title, style: TextStyle(color: Colors.white)),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(subtitle, style: TextStyle(color: Colors.white70)),
-              SizedBox(height: 8),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(value, style: TextStyle(color: Colors.white)),
-                  Row(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          // Add your edit logic here
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor:const Color(0xFF27D0C6), // Blue background color
-                          shape: CircleBorder(), // Make the button circular
-                        ),
-                        child: Icon(Icons.edit, color: Colors.white, size: 18), // Adjust the size of the icon
+      child: ListTile(
+        title: Text(title, style: TextStyle(color: Colors.white)),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(subtitle, style: TextStyle(color: Colors.white70)),
+            SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(value, style: TextStyle(color: Colors.white)),
+                Row(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        openEditDialog(title, subtitle, value);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            Color(0xFF27D0C6), // Blue background color
+                        shape: CircleBorder(), // Make the button circular
+                        minimumSize: Size(36, 36), // Set the size of the button
                       ),
-                      SizedBox(width: 8),
-                      ElevatedButton(
-                        onPressed: () {
-                          // Add your delete logic here
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFFF2003D), // Red background color
-                          shape: CircleBorder(), // Make the button circular
-                        ),
-                        child: Icon(Icons.delete, color: Colors.white, size: 18), // Adjust the size of the icon
+                      child: Icon(Icons.edit,
+                          color: Colors.white,
+                          size: 18), // Adjust the size of the icon
+                    ),
+                    SizedBox(width: 8),
+                    ElevatedButton(
+                      onPressed: () {
+                        openDeleteDialog(title);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFFF2003D),
+                        shape: CircleBorder(),
+                        minimumSize: Size(36, 36),
                       ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
+                      child: Icon(Icons.delete, color: Colors.white, size: 18),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
+
+  Future openCreateDialog() => showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("Crear Registro"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Ingrese el nombre del movimiento",
+                ),
+              ),
+              SizedBox(height: 8), // Espacio entre los TextFields
+              TextField(
+                maxLines: 3, // Ajusta el número de líneas según sea necesario
+                decoration: InputDecoration(
+                  hintText: "Ingrese la descripción del movimiento",
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Ingrese el valor del movimiento",
+                ),),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text("Cancelar"),
+            ),
+            TextButton(
+              onPressed: () {
+                // Lógica para crear el registro
+                Navigator.of(context).pop();
+              },
+              child: Text("Crear"),
+            ),
+          ],
+        ),
+      );
+
+  Future openEditDialog(String title, String subtitle, String value) => showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("Editar Registro"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Ingrese el nuevo nombre del movimiento",
+                ),
+                controller: TextEditingController(text: title),
+              ),
+              SizedBox(height: 8), // Espacio entre los TextFields
+              TextField(
+                maxLines: 3, // Ajusta el número de líneas según sea necesario
+                decoration: InputDecoration(
+                  hintText: "Ingrese la nueva descripción del movimiento",
+                ),
+                controller: TextEditingController(text: subtitle),
+              ),
+              SizedBox(height: 8), // Espacio entre los TextFields
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Ingrese el nuevo valor del movimiento",
+                ),
+                controller: TextEditingController(text: value),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text("Cancelar"),
+            ),
+            TextButton(
+              onPressed: () {
+                // Lógica para editar el registro
+                Navigator.of(context).pop();
+              },
+              child: Text("Guardar"),
+            ),
+          ],
+        ),
+      );
+
+  Future openDeleteDialog(String title) => showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("Eliminar Registro"),
+          content: Text("¿Está seguro que desea eliminar el movimiento \"$title\"?"),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text("Cancelar"),
+            ),
+            TextButton(
+              onPressed: () {
+                // Lógica para eliminar el registro
+                Navigator.of(context).pop();
+              },
+              child: Text("Eliminar"),
+            ),
+          ],
+        ),
+      );
 }
